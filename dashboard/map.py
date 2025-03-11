f901ba99e2d68f46b1e67eef91288b62831ba15b
atharv-vasisht <atharvgurupurna@gmail.com>
2025-03-11 14:47:35 +0100

    fixed map.py

--- dashboard/map.py
+++ dashboard/map.py
@@ -145,5 +145,7 @@ def update_map(selected_date, animation_enabled):
 
     return fig
 
+'''
 if __name__ == "__main__":
-    app.run_server(debug=True, host="127.0.0.1", port=8080)
+    app.run_server(debug=True, host="127.0.0.1", port=8080)'
+    '''
